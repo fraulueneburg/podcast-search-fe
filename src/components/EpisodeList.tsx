da65d07b27c6bd64619b81874ddb87869debe284
fraulueneburg <github@fraulueneburg.de>
2025-07-11 23:49:21 +0200

    feat (EpisodeList): add duration

--- src/components/EpisodeList.tsx
+++ src/components/EpisodeList.tsx
@@ -12,6 +12,7 @@ export default function EpisodeList(props) {
 			'#text': string
 		}
 		image: string
+		duration: string
 	}
 
 	return (
@@ -19,11 +20,13 @@ export default function EpisodeList(props) {
 			<h3>EpisodeList</h3>
 			{episodes?.length > 0 && (
 				<ul className="list-episodes">
-					{episodes.map(({ title, description, pubDate, guid, image }: EpisodeListType) => (
+					{episodes.map(({ title, description, pubDate, guid, image, duration }: EpisodeListType) => (
 						<li className={guid?.['#text']}>
 							{image && <img src={image} width="150" alt={title} />}
 							<div>
-								<small>{pubDate}</small>
+								<small>
+									{pubDate} | {duration}
+								</small>
 								<h4>{title}</h4>
 								<div className="desc">{parse(description)}</div>
 							</div>
