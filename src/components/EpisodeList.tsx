16565422a4e923efbbdeecbdfb44ba4c4cfed2ce
fraulueneburg <github@fraulueneburg.de>
2025-07-15 19:34:36 +0200

    fix: update episode type

--- src/components/EpisodeList.tsx
+++ src/components/EpisodeList.tsx
@@ -5,17 +5,14 @@ export default function EpisodeList(props) {
 	const episodes = props.data
 	const { podcastId } = props
 
-	type EpisodeListType = {
+	type EpisodeType = {
 		title: string
 		description: string
 		pubDate: string
-		guid: {
-			'@_isPermaLink': string
-			'#text': string
-		}
+		guid: string
+		audioURL: string
 		image: string
 		duration: string
-		audioURL: string
 	}
 
 	return (
@@ -23,7 +20,7 @@ export default function EpisodeList(props) {
 			<h3>EpisodeList</h3>
 			{episodes?.length > 0 && (
 				<ul className="list-episodes">
-					{episodes.map(({ title, description, pubDate, guid, image, duration, audioURL }: EpisodeListType) => (
+					{episodes.map(({ title, description, pubDate, guid, image, duration, audioURL }: EpisodeType) => (
 						<li key={guid?.['#text']}>
 							<div>{image && <img src={image} width="150" alt={title} />}</div>
 							<div>
