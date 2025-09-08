ac06b874c5cabbb5696d48690a4496c40b540621
fraulueneburg <github@fraulueneburg.de>
2025-09-08 19:52:36 +0200

    reformat (styles): css to scss

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { RouterProvider } from '@tanstack/react-router'
 import { router } from './router'
-import './styles/main.css'
+import './assets/scss/styles.scss'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
 	<React.StrictMode>
