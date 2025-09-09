47c989f755c3bcc9f474bfb78aae706ab35bfec1
fraulueneburg <github@fraulueneburg.de>
2025-09-09 18:55:28 +0200

    refactor (Menu): add links

--- src/components/Menu/Menu.tsx
+++ src/components/Menu/Menu.tsx
@@ -1,5 +1,5 @@
 import './Menu.scss'
-import { Link } from '@tanstack/react-router'
+import { Link, useMatchRoute } from '@tanstack/react-router'
 
 import {
 	HouseLineIcon as IconHome,
@@ -8,35 +8,35 @@ import {
 	UserIcon as IconProfile,
 } from '@phosphor-icons/react'
 
+const navItems = [
+	{ to: '/', label: 'Home', icon: IconHome },
+	{ to: '/library', label: 'Library', icon: IconLibrary },
+	{ to: '/saved', label: 'Saved', icon: IconSaved },
+	{ to: '/profile', label: 'Profile', icon: IconProfile },
+]
+
+function NavLink({ to, label, icon: Icon }: { to: string; label: string; icon: React.ElementType }) {
+	const matchRoute = useMatchRoute()
+	const isActive = !!matchRoute({ to, fuzzy: false })
+
+	return (
+		<li>
+			<Link to={to} className={isActive ? 'current' : ''} aria-current={isActive ? 'page' : undefined}>
+				<Icon />
+				{label}
+			</Link>
+		</li>
+	)
+}
+
 export function Menu() {
 	return (
 		<>
 			<nav className="menu-main">
 				<ul>
-					<li>
-						<Link to="/">
-							<IconHome />
-							Home
-						</Link>
-					</li>
-					<li>
-						<a href="">
-							<IconLibrary />
-							Library
-						</a>
-					</li>
-					<li>
-						<a href="">
-							<IconSaved />
-							Saved
-						</a>
-					</li>
-					<li>
-						<a href="">
-							<IconProfile />
-							Profile
-						</a>
-					</li>
+					{navItems.map((elem) => (
+						<NavLink key={elem.to} to={elem.to} label={elem.label} icon={elem.icon} />
+					))}
 				</ul>
 			</nav>
 		</>
